9897389240319472660f30c9b6adc8181e7b4562
Gwénaël <gwenael.mahe@laplateforme.io>
2021-10-05 14:51:27 +0200

    test non fonctionnel

---
+++ main.py
@@ -0,0 +1,50 @@
+import random
+
+
+class Maze:
+    def __init__(self, width, height):
+        self.width = width // 2 * 2 - 1
+        self.height = height // 2 * 2 - 1
+        self.cells = [[True for x in range(self.width)] for y in range(self.height)]
+
+    def set_path(self, x, y):
+        self.cells[x, y] = False
+
+    def set_walls(self, x, y):
+        self.cells[x, y] = True
+
+    def is_wall(self, x, y):
+        if 0 <= x < self.width and 0 <= y < self.height:
+            return self.cells[y][x]
+        else:
+            return "."
+
+    def create_maze(self, x, y):
+        self.set_path(x, y)
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        random.shuffle(directions)
+        while len(directions) > 0:
+            movemement = directions.pop()
+            node_x = x + (movemement[0] * 2)
+            node_y = y + (movemement[1] * 2)
+            link_cells_x = x + movemement[0]
+            link_cells_y = y + movemement[1]
+            self.set_path(link_cells_x, link_cells_y)
+            self.create_maze(node_x, node_y)
+        return
+
+    def __str__(self):
+        string = ""
+        conv = {
+            True: "#",
+            False: "."
+        }
+        for y in range(self.height):
+            for x in range(self.width):
+                string += conv[self.cells[y][x]]
+            string += "\n"
+        return string
+
+
+Maze.create_maze(10, 10)
+print(Maze)
