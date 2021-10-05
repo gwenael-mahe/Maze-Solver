ba6eea4ce8a8c8d8bfac0dd93dcc48e74810a7da
Benjamin Bigot <91526806+BenCharo@users.noreply.github.com>
2021-10-05 17:03:06 +0200

    Tentative amélioration classe Maze

--- maze.py
+++ maze.py
@@ -25,16 +25,22 @@ class Maze:
         else:
             return "."
 
+    def is_visited(self, x, y):
+        if self.cells[y][x] == "":
+            return False
+        else:
+            return True
+
     def create_maze(self, x, y):
         self.set_path(x, y)
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         random.shuffle(directions)
         while len(directions) > 0:
-            movemement = directions.pop()
-            node_x = x + (movemement[0] * 2)
-            node_y = y + (movemement[1] * 2)
-            link_cells_x = x + movemement[0]
-            link_cells_y = y + movemement[1]
+            movement = directions.pop()
+            node_x = x + (movement[0] * 2)
+            node_y = y + (movement[1] * 2)
+            link_cells_x = x + movement[0]
+            link_cells_y = y + movement[1]
             self.set_path(link_cells_x, link_cells_y)
             self.create_maze(node_x, node_y)
         return
