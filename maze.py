2dbf558c9181b4bac523422a3d24089f2dc482a0
Benjamin Bigot <91526806+BenCharo@users.noreply.github.com>
2021-10-05 15:17:34 +0200

    Transformation du labyrinthe en tant que classe

--- maze.py
+++ maze.py
@@ -1,20 +1,24 @@
 import random
 
+class Maze:
 
-def initialize_maze(x):
-    rows, columns = (2 * x + 1, 2 * x + 1)
-    maze = [["." for i in range(rows)] for j in range(columns)]
+    def __init__(self, taille):
+        self.width = (2 * taille + 1)
+        self.grid = [["0" for i in range(self.width)] for j in range(self.width)]
 
-    # for row in maze:
-    #   print(row)
+    def set_path(self, x, y):
+        self.grid[y][x] = "."
 
-    return maze
+    def set_wall(self, x, y):
+        self.grid[y][x] = "#"
 
+    def is_visited(self, x, y):
+        if self.grid[y][x] == "0":
+            return False
+        else:
+            return True
 
-def test(x, y, maze):
-    directions = ["N", "S", "E", "W"]
-    random.shuffle(directions)
-    print(directions)
+    def direction(self):
+        direction = ["N", "S", "E", "W"]
+        return random.shuffle(direction)
 
-
-test(0, 0, initialize_maze(5))
