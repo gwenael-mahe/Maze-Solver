4e4bd5bb6217a995aa9d8ed032c54bdf9533d787
Benjamin Bigot <91526806+BenCharo@users.noreply.github.com>
2021-10-05 14:49:34 +0200

    ajout de la randomisation des points cardinaux

--- maze.py
+++ maze.py
@@ -1,9 +1,20 @@
+import random
+
+
 def initialize_maze(x):
     rows, columns = (2 * x + 1, 2 * x + 1)
     maze = [["." for i in range(rows)] for j in range(columns)]
 
-    for row in maze:
-        print(row)
+    # for row in maze:
+    #   print(row)
+
+    return maze
 
 
-initialize_maze(5)
+def test(x, y, maze):
+    directions = ["N", "S", "E", "W"]
+    random.shuffle(directions)
+    print(directions)
+
+
+test(0, 0, initialize_maze(5))
