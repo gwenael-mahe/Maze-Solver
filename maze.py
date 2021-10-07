c11c9be6dca9e36840b55ef8304f5682defcd18f
Benjamin Bigot <91526806+BenCharo@users.noreply.github.com>
2021-10-07 14:40:49 +0200

    Debut Kruskal, remplissage des cases du labyrinthe pour un arbre (non opérationnel)

--- maze.py
+++ maze.py
@@ -11,6 +11,8 @@ class Maze:
         #self.set_walls()
         self.cells[0][0] = "."
         self.cells[self.width - 1][self.height - 1] = "."
+
+        # Génération de l'entrée et de la sortie
         rand = random.randint(1, 2)
         if rand == 1:
             self.cells[0][1] = "."
@@ -44,10 +46,10 @@ class Maze:
                     self.cells[j][i] = "#"
 
     def is_visited(self, x, y):
-        if self.cells[y][x] == "0":
-            return False
-        else:
+        if self.cells[y][x] == ".":
             return True
+        else:
+            return False
 
     def recursive_backtrack(self, x, y):
         self.set_path(x, y)
@@ -72,24 +74,33 @@ class Maze:
         return True
 
     def kruskal(self):
+        step = 1
         for i in range(self.width):
             for j in range(self.height):
                 if i % 2 == 0 or j % 2 == 0:
                     self.cells[j][i] = "#"
                 else:
-                    self.cells[j][i] = "."
- #       while self.all_visited() is False:
- #           x = random.randint(0, self.width - 1)
- #           y = random.randint(0, self.height - 1)
+                    self.cells[j][i] = step
+                    step += 1
+        while self.all_visited() is False:
+            x = random.randint(0, self.width - 1)
+            y = random.randint(0, self.height - 1)
 
- #           directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
- #           random.shuffle(directions)
- #           while len(directions) > 0:
- #               movement = directions.pop()
- #               node_x = x + (movement[0] * 2)
- #               node_y = y + (movement[1] * 2)
- #               link_cells_x = x + movement[0]
- #               link_cells_y = y + movement[1]
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            random.shuffle(directions)
+            while len(directions) > 0:
+                movement = directions.pop()
+                node_x = x + (movement[0] * 2)
+                node_y = y + (movement[1] * 2)
+                link_cells_x = x + movement[0]
+                link_cells_y = y + movement[1]
+                if 0 < node_x < self.width and 0 < node_y < self.height and self.is_visited(node_x, node_y) is False:
+                    self.set_path(link_cells_x, link_cells_y)
+                    if self.cells[y][x] > self.cells[node_y][node_x]:
+                        self.cells[y][x] = self.cells[node_y][node_x]
+                    else:
+                        self.cells[node_y][node_x] = self.cells[y][x]
+                    break
 
 
 X = Maze(4)
