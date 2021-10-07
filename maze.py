89f8ddd9255620d2b2c63d63c2e51a8e751ddd23
Benjamin Bigot <91526806+BenCharo@users.noreply.github.com>
2021-10-07 16:33:10 +0200

    Ajout des méthodes d'écriture dans un fichier pour le recursive backtrack

--- maze.py
+++ maze.py
@@ -4,15 +4,15 @@ import random
 class Maze:
     def __init__(self, width):
         self.width = 2 * width + 1  # // 2 * 2 - 1
-        self.height = 2 * width + 1     # // 2 * 2 - 1
-        self.cells = [["0" for x in range(self.width)] for y in range(self.height)]
-        #self.recursive_backtrack(1, 1)
-        self.kruskal()
-        #self.set_walls()
+        self.cells = [["0" for x in range(self.width)] for y in range(self.width)]
+        self.visited = []
+        self.recursive_backtrack(1, 1)
+        # self.kruskal()
+        self.set_walls()
         self.cells[0][0] = "."
-        self.cells[self.width - 1][self.height - 1] = "."
+        self.cells[self.width - 1][self.width - 1] = "."
 
-        # Génération de l'entrée et de la sortie
+        # Generation de l' entree et de la sortie
         rand = random.randint(1, 2)
         if rand == 1:
             self.cells[0][1] = "."
@@ -20,39 +20,48 @@ class Maze:
             self.cells[1][0] = "."
         rand = random.randint(1, 2)
         if rand == 1:
-            self.cells[self.width - 1][self.height - 2] = "."
+            self.cells[self.width - 1][self.width - 2] = "."
         else:
-            self.cells[self.width - 2][self.height - 1] = "."
+            self.cells[self.width - 2][self.width - 1] = "."
 
     def display(self):
+        fichier = open("data.txt", "w")
         for i in range(self.width):
-            for j in range(self.height):
+            for j in range(self.width):
+                fichier.write(self.cells[j][i])
                 print(self.cells[j][i], end=' ')
             print("\n")
+            fichier.write("\n")
+        fichier.close()
 
     def set_path(self, x, y):
         self.cells[y][x] = "."
 
     def is_wall(self, x, y):
-        if 0 <= x < self.width and 0 <= y < self.height:
+        if 0 <= x < self.width and 0 <= y < self.width:
             return self.cells[y][x]
         else:
             return "."
 
     def set_walls(self):
         for i in range(self.width):
-            for j in range(self.height):
+            for j in range(self.width):
                 if self.cells[j][i] == "0":
                     self.cells[j][i] = "#"
 
+    def set_visited(self, x, y):
+        self.visited.append((x, y))
+        list(set(self.visited))
+
     def is_visited(self, x, y):
-        if self.cells[y][x] == ".":
+        if (x, y) in self.visited:
             return True
         else:
             return False
 
     def recursive_backtrack(self, x, y):
         self.set_path(x, y)
+        self.set_visited(x, y)
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         random.shuffle(directions)
         while len(directions) > 0:
@@ -67,16 +76,15 @@ class Maze:
         return
 
     def all_visited(self):
-        for i in range(self.width - 1):
-            for j in range(self.height - 1):
-                if self.is_visited(i, j) is False:
-                    return False
-        return True
+        if len(self.visited) == self.width * self.width:
+            return True
+        else:
+            return False
 
     def kruskal(self):
         step = 1
         for i in range(self.width):
-            for j in range(self.height):
+            for j in range(self.width):
                 if i % 2 == 0 or j % 2 == 0:
                     self.cells[j][i] = "#"
                 else:
@@ -84,8 +92,7 @@ class Maze:
                     step += 1
         while self.all_visited() is False:
             x = random.randint(0, self.width - 1)
-            y = random.randint(0, self.height - 1)
-
+            y = random.randint(0, self.width - 1)
             directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
             random.shuffle(directions)
             while len(directions) > 0:
@@ -94,7 +101,7 @@ class Maze:
                 node_y = y + (movement[1] * 2)
                 link_cells_x = x + movement[0]
                 link_cells_y = y + movement[1]
-                if 0 < node_x < self.width and 0 < node_y < self.height and self.is_visited(node_x, node_y) is False:
+                if 0 < node_x < self.width and 0 < node_y < self.width and self.is_visited(node_x, node_y) is False:
                     self.set_path(link_cells_x, link_cells_y)
                     if self.cells[y][x] > self.cells[node_y][node_x]:
                         self.cells[y][x] = self.cells[node_y][node_x]
@@ -103,5 +110,5 @@ class Maze:
                     break
 
 
-X = Maze(4)
+X = Maze(40)
 Maze.display(X)
