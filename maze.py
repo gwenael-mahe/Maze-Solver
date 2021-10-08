3d1f6ad9f1bd1be536ec8c42ca1f543a5afa043b
Benjamin Bigot <91526806+BenCharo@users.noreply.github.com>
2021-10-08 10:35:43 +0200

    Kruskal (pas encore opérationnel)

--- maze.py
+++ maze.py
@@ -6,9 +6,9 @@ class Maze:
         self.width = 2 * width + 1  # // 2 * 2 - 1
         self.cells = [["0" for x in range(self.width)] for y in range(self.width)]
         self.visited = []
-        self.recursive_backtrack(1, 1)
-        # self.kruskal()
-        self.set_walls()
+        # self.recursive_backtrack(1, 1)
+        self.kruskal()
+        # self.set_walls()
         self.cells[0][0] = "."
         self.cells[self.width - 1][self.width - 1] = "."
 
@@ -87,12 +87,17 @@ class Maze:
             for j in range(self.width):
                 if i % 2 == 0 or j % 2 == 0:
                     self.cells[j][i] = "#"
+                    self.set_visited(i, j)
                 else:
                     self.cells[j][i] = step
                     step += 1
         while self.all_visited() is False:
-            x = random.randint(0, self.width - 1)
-            y = random.randint(0, self.width - 1)
+            x = 0
+            y = 0
+            while x % 2 == 0:
+                x = random.randint(1, self.width - 2)
+            while y % 2 == 0:
+                y = random.randint(1, self.width - 2)
             directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
             random.shuffle(directions)
             while len(directions) > 0:
@@ -101,14 +106,27 @@ class Maze:
                 node_y = y + (movement[1] * 2)
                 link_cells_x = x + movement[0]
                 link_cells_y = y + movement[1]
-                if 0 < node_x < self.width and 0 < node_y < self.width and self.is_visited(node_x, node_y) is False:
+                if 0 < node_x < self.width and 0 < node_y < self.width:
                     self.set_path(link_cells_x, link_cells_y)
+                    self.set_visited(x, y)
+                    self.set_visited(node_x, node_y)
                     if self.cells[y][x] > self.cells[node_y][node_x]:
-                        self.cells[y][x] = self.cells[node_y][node_x]
-                    else:
-                        self.cells[node_y][node_x] = self.cells[y][x]
-                    break
+                        for i in range(self.width):
+                            for j in range(self.width):
+                                if self.cells[j][i] == self.cells[y][x]:
+                                    self.cells[j][i] = self.cells[node_y][node_x]
+                        break
+                    elif self.cells[y][x] < self.cells[node_y][node_x]:
+                        for i in range(self.width):
+                            for j in range(self.width):
+                                if self.cells[j][i] == self.cells[node_y][node_x]:
+                                    self.cells[j][i] = self.cells[y][x]
+                        break
+        for i in range(self.width):
+            for j in range(self.width):
+                if self.cells[j][i] != "#":
+                    self.cells[j][i] = "."
 
 
-X = Maze(40)
+X = Maze(4)
 Maze.display(X)
