a818258c70940d5e9bf8ca7b0ee64337c4b5c6de
Gwénaël <gwenael.mahe@laplateforme.io>
2021-10-18 08:45:16 +0200

    modif resolution

--- maze.py
+++ maze.py
@@ -3,13 +3,20 @@ import sys
 
 sys.setrecursionlimit(15000)
 
+
 class Maze:
     def __init__(self, width):
         self.width = 2 * width + 1  # // 2 * 2 - 1
-        self.height = 2 * width + 1  # // 2 * 2 - 1
-        self.cells = [["0" for x in range(self.width)] for y in range(self.height)]
+        self.cells = [["0" for x in range(self.width)] for y in range(self.width)]
+        self.visited = []
+        self.final_path = []
+        self.recursive_backtrack(1, 1)
+        # self.kruskal()
+        self.set_walls()
         self.cells[0][0] = "."
-        self.cells[self.width - 1][self.height - 1] = "."
+        self.cells[self.width - 1][self.width - 1] = "."
+
+        # Generation de l' entree et de la sortie
         rand = random.randint(1, 2)
         if rand == 1:
             self.cells[0][1] = "."
@@ -17,44 +24,49 @@ class Maze:
             self.cells[1][0] = "."
         rand = random.randint(1, 2)
         if rand == 1:
-            self.cells[self.width - 1][self.height - 2] = "."
+            self.cells[self.width - 1][self.width - 2] = "."
         else:
-            self.cells[self.width - 2][self.height - 1] = "."
-        self.recursive_backtrack(1, 1)
-        self.set_walls()
+            self.cells[self.width - 2][self.width - 1] = "."
+        self.visited.clear()
 
     def display(self):
+        file = open(input("Veuillez rentrer le nom du fichier : "), "w")
         for i in range(self.width):
-            for j in range(self.height):
+            for j in range(self.width):
+                file.write(self.cells[j][i])
                 print(self.cells[j][i], end=' ')
             print("\n")
-
-    def get_cells(self):
-        return self.cells
+            file.write("\n")
+        file.close()
 
     def set_path(self, x, y):
         self.cells[y][x] = "."
 
     def is_wall(self, x, y):
-        if 0 <= x < self.width and 0 <= y < self.height:
+        if 0 <= x < self.width and 0 <= y < self.width:
             return self.cells[y][x]
         else:
             return "."
 
     def set_walls(self):
         for i in range(self.width):
-            for j in range(self.height):
+            for j in range(self.width):
                 if self.cells[j][i] == "0":
                     self.cells[j][i] = "#"
 
+    def set_visited(self, x, y):
+        self.visited.append((x, y))
+        list(set(self.visited))
+
     def is_visited(self, x, y):
-        if self.cells[y][x] == "0":
-            return False
-        else:
+        if (x, y) in self.visited:
             return True
+        else:
+            return False
 
     def recursive_backtrack(self, x, y):
         self.set_path(x, y)
+        self.set_visited(x, y)
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         random.shuffle(directions)
         while len(directions) > 0:
@@ -69,44 +81,110 @@ class Maze:
         return
 
     def all_visited(self):
-        for i in range(self.width - 1):
-            for j in range(self.height - 1):
+        for i in range(self.width):
+            for j in range(self.width):
                 if self.is_visited(i, j) is False:
                     return False
         return True
 
     def kruskal(self):
-        while self.all_visited() is False:
-            x = random.randint(0, self.width - 1)
+        step = 1
+        node = {}
+        for i in range(self.width):
+            for j in range(self.width):
+                if i % 2 == 0 or j % 2 == 0:
+                    self.cells[j][i] = "#"
+                    self.set_visited(i, j)
+                else:
+                    self.cells[j][i] = step
+                    node[step] = [(i, j)]
+                    step += 1
+        while self.all_visited() is False and len(node) != 1:
+            x = 0
+            y = 0
+            while x % 2 == 0:
+                x = random.randint(1, self.width - 2)
+            while y % 2 == 0:
+                y = random.randint(1, self.width - 2)
             directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
             random.shuffle(directions)
-            deplacement = directions.pop()
-            y = x + (deplacement[0] * 2)
             while len(directions) > 0:
-                y = False
+                movement = directions.pop()
+                node_x = x + (movement[0] * 2)
+                node_y = y + (movement[1] * 2)
+                link_cells_x = x + movement[0]
+                link_cells_y = y + movement[1]
+                if 0 < node_x < self.width - 1 and 0 < node_y < self.width - 1:
+                    if self.cells[y][x] != self.cells[node_y][node_x]:
+                        self.set_path(link_cells_x, link_cells_y)
+                        self.set_visited(x, y)
+                        self.set_visited(node_x, node_y)
+                    if self.cells[y][x] > self.cells[node_y][node_x]:
+                        tmp = self.cells[y][x]
+                        for i in node[self.cells[y][x]]:
+                            node[self.cells[node_y][node_x]].append(i)
+                        del node[self.cells[y][x]]
+                        for i in range(self.width):
+                            for j in range(self.width):
+                                if self.cells[j][i] == tmp:
+                                    self.cells[j][i] = self.cells[node_y][node_x]
+                        break
+                    elif self.cells[y][x] < self.cells[node_y][node_x]:
+                        tmp = self.cells[node_y][node_x]
+                        for i in node[self.cells[node_y][node_x]]:
+                            node[self.cells[y][x]].append(i)
+                        del node[self.cells[node_y][node_x]]
+                        for i in range(self.width):
+                            for j in range(self.width):
+                                if self.cells[j][i] == tmp:
+                                    self.cells[j][i] = self.cells[y][x]
+                        break
+        for i in range(self.width):
+            for j in range(self.width):
+                if self.cells[j][i] != "#":
+                    self.cells[j][i] = "."
+
+    def recursive_backtrack_resolution(self, position):
+        x, y = position
+        self.set_visited(x, y)
+        if position == (self.width - 1, self.width - 1):
+            self.final_path.append(position)
+            return
+        if x + 1 < self.width and self.cells[y][x + 1] == '.' and self.is_visited(x + 1, y) is False:
+            self.recursive_backtrack_resolution((x + 1, y))
+            if (self.width - 1, self.width - 1) in self.final_path:
+                self.final_path.append(position)
+            return
+
+        if y + 1 < self.width and self.cells[y + 1][x] == '.' and self.is_visited(x, y + 1) is False:
+            self.recursive_backtrack_resolution((x, y + 1))
+            if (self.width - 1, self.width - 1) in self.final_path:
+                self.final_path.append(position)
+            return
+
+        if x - 1 < self.width and self.cells[y][x - 1] == '.' and self.is_visited(x - 1, y) is False:
+            self.recursive_backtrack_resolution((x - 1, y))
+            if (self.width - 1, self.width - 1) in self.final_path:
+                self.final_path.append(position)
+            return
+
+        if y - 1 < self.width and self.cells[y - 1][x] == '.' and self.is_visited(x, y - 1) is False:
+            self.recursive_backtrack_resolution((x, y - 1))
+            if (self.width - 1, self.width - 1) in self.final_path:
+                self.final_path.append(position)
+            return
+
+    def set_final_path(self):
+        for i in range(self.width):
+            for j in range(self.width):
+                if (i, j) in self.final_path:
+                    self.cells[j][i] = "o"
+                if (i, j) not in self.final_path and self.cells[j][i] != "#":
+                    self.cells[j][i] = "*"
+        return True
 
 
-def print_solution(sol):
-    file = open(input("veuillez rentrer le nom du fichier : "), "w")
-    for i in sol:
-        for j in i:
-            file.write(str(j) + " ", end="")
-        file.write("\n")
-    file.close()
-
-def solve_maze(maze, N):
-    sol = [[0 for j in range(N)] for i in range(N)]
-    if not solve_maze_recursive(maze, 0, 0, sol):
-        print("Il n'y a pas de solution.")
-        return False
-    print_solution(sol)
-    return True
-
-def solve_maze_recursive(maze, x, y, sol):
-    #test
-
-
-X = Maze(4)
-print(X.get_cells())
-# Maze.display(X)
-# print(solve_maze(X.get_cells(), [0, 0], 9))
+X = Maze(10)
+X.recursive_backtrack_resolution((0, 0))
+X.set_final_path()
+X.display()
